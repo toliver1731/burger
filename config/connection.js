eb127752a286a4e85071fc47403a63cdda738634
Tommy Oliver <Tommy Oliver>
2017-10-29 18:59:52 -0600

    updated concection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: 'localhost',
+var connection;
+// create mysql connection
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
 		user: 'root',
-		password: 'Spotogo31!',
+		password: 'password',
 		database: 'burger_db'
-});
+	});
+}
 
+// Connect to the mySQL database
 connection.connect(function(err) {
-	if(err)throw err;
-	console.log("connection as id: " + connection.threadid);
+    if (err) throw err;
+    console.log("Connected as ID: " + connection.threadId);
 });
 
+// Export connection to ORM
 module.exports = connection;
-
